703be8825fab5a5bbbe7970eeff4a77603814944
Thomas Rayner <thmsrynr@outlook.com>
2018-03-28 14:04:31 -0600

    First commit of json template and config script

---
+++ Initialize-DevWorkstation.ps1
@@ -0,0 +1,32 @@
+[cmdletbinding()]
+param (
+    [switch]$All,
+    [switch]$Office,
+    [switch]$PowerPointViewer,
+    [switch]$VisualStudio,
+    [switch]$Help,
+    [switch]$Remoting
+)
+
+Set-ExecutionPolicy Bypass -Scope Process -Force
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco install powershell -y
+
+choco install googlechrome 7zip git.install visualstudiocode vscode-powershell conemu -y
+
+if ( $All -or $VisualStudio ) { choco install visualstudio2017enterprise -y }
+
+if ( $PowerPointViewer ) { choco install powerpoint.viewer -y }
+
+if ( $All -or $Office ) { choco install office365proplus -y }
+
+if ( $All -or $Help ) { update-help }
+
+if ( $All -or $Remoting ) { 
+    Enable-PSRemoting –force
+    Set-Service WinRM -StartMode Automatic
+    get-ciminstance -ClassName win32_service | where-object { $_.Name -eq 'WinRM' }
+    Set-Item WSMan:localhost\client\trustedhosts -value * # this is SUPER sketchy so use at your own risk
+    Get-Item WSMan:\localhost\Client\TrustedHosts
+}
