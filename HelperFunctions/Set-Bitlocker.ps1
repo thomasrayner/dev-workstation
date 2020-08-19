b751ee199adf2c18cf6d6aab1249a67e70bdcfd1
Nate Bruneau <nathbr@microsoft.com>
2020-08-19 14:42:38 -0700

    Revert "addign set bitlocker helper, wip"
    This reverts commit 0d76a632480abd57def089f277a3647f55d9a6f1.

--- HelperFunctions/Set-Bitlocker.ps1
+++
@@ -1,3 +0,0 @@
-$KeyVault = Get-AzKeyVault -VaultName NVault2020 -ResourceGroupName Vault 
-
-Set-AzVMDiskEncryptionExtension -ResourceGroupName "2020-08-18-Nate" -VMName "nate-dev" -DiskEncryptionKeyVaultUrl $KeyVault.VaultUri -DiskEncryptionKeyVaultId $KeyVault.ResourceId
