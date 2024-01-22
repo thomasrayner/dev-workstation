b978e28ed8568f51d89df4f38f35c564e15f7ca3
Thomas Rayner <thmsrynr@outlook.com>
2024-01-22 12:23:38 -0800

    Update Initialize-LinuxDevWorkstation.sh

--- Initialize-LinuxDevWorkstation.sh
+++ Initialize-LinuxDevWorkstation.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+sudo apt-get install -y wget apt-transport-https software-properties-common
+source /etc/os-release
+wget -q https://packages.microsoft.com/config/ubuntu/$VERSION_ID/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+sudo apt-get update
 sudo apt-get install -y powershell
 
 # Set PowerShell as the default shell
