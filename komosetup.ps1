ad49057b32022c1fc3fbdeb85f5fc849977bac53
Thomas Rayner <thmsrynr@outlook.com>
2024-03-01 15:39:43 -0800

    komorebi tweaks

--- komosetup.ps1
+++ komosetup.ps1
@@ -2,5 +2,5 @@ Get-Process *whkd* | Stop-Process
 Start-Process -FilePath "whkd --config $env:USERPROFILE\.config\whkdrc" -WindowStyle Hidden
 Get-Process *komo* | Stop-Process
 komorebic start --whkd
-komorebic active-window-border-width 5
 komorebic mouse-follows-focus disable
+komorebic active-window-border-width 50
